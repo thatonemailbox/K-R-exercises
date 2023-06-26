69ed0e02fa1da1af8225845274c3e88be64ebd3f
thatonemailbox <127370142+thatonemailbox@users.noreply.github.com>
2023-06-26 16:32:28 -0400

    made this work more like the examples

--- 1/1-8/1-8.c
+++ 1/1-8/1-8.c
@@ -5,19 +5,16 @@ int main(void) {
 	int c;	
 
 	int ns = 0, nt = 0, nl = 0;
-	while (1) {
-		if ((c = getchar()) != EOF) {
-			if (c == ' ')  {
-				ns++;
-			}
-			if (c == '\t') {
-				++nt;
-			}	
-			if (c == '\n') {
-				nl++;
-			}
-		} else {
-			printf("Spaces: %d\nTabs: %d\nLines: %d\n", ns, nt, nl);
+	while ((c = getchar()) != EOF) {
+		if (c == ' ')  {
+			ns++;
+		}
+		if (c == '\t') {
+			++nt;
+		}	
+		if (c == '\n') {
+			nl++;
 		}
 	}
+	printf("Spaces: %d\nTabs: %d\nLines: %d\n", ns, nt, nl);
 }
