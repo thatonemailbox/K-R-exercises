ddf086b9ab8ea09f32573831933e1c6cf182496f
thatonemailbox <127370142+thatonemailbox@users.noreply.github.com>
2023-06-26 16:09:32 -0400

    ok

--- 1/1-8/1-8.c
+++ 1/1-8/1-8.c
@@ -2,13 +2,22 @@
 
 /* count blanks, tabs, and lines in input */
 int main(void) {
-	int c;
-	
-	int count = 0;
-	while ((c = getchar()) != EOF) {
-		if (c == ' ' || c == '\t' || c == '\n') {
-			count++;
+	int c;	
+
+	int ns = 0, nt = 0, nl = 0;
+	while (1) {
+		if ((c = getchar()) != EOF) {
+			if (c == ' ')  {
+				ns++;
+			}
+			if (c == '\t') {
+				++nt;
+			}	
+			if (c == '\n') {
+				nl++;
+			}
+		} else {
+			printf("Spaces: %d\nTabs: %d\nLines: %d\n", ns, nt, nl);
 		}
 	}
-	printf("%i", count);
 }
